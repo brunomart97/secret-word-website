a7bcb38bcc8acf6212b5b8af495f87be1f130619
Bruno Martins <brunomart97@hotmail.com>
2024-03-12 20:25:03 -0300

    feat: function to generate unique ID

---
+++ src/utils/idGenerator.ts
@@ -0,0 +1,9 @@
+export const idGenerator = (text: string): string => {
+  const randomString = Math.random().toString(16).substring(2, 8)
+  const combinedString = `${text}-${randomString}`
+  const hash = require('crypto')
+    .createHash('md5')
+    .update(combinedString)
+    .digest('hex')
+  return hash.substr(0, 8)
+}
