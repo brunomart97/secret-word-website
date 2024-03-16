3596294e8a81a39a5c1f9f81ce49dcc36c165360
Bruno Martins <brunomart97@hotmail.com>
2024-03-16 17:01:23 -0300

    feat: game banner texts

--- src/components/ui/GameBanner/index.tsx
+++ src/components/ui/GameBanner/index.tsx
@@ -1,16 +1,17 @@
+import { useTranslations } from 'next-intl'
+
 import styles from './styles.module.scss'
 
 export const GameBanner = () => {
+  const i18n = useTranslations('i18n')
+
   return (
     <div className={styles.gameBannerContainer}>
       <div className={styles.gameBannerSubcontainer}>
         <div className={styles.gameBannerLeftContent}>
-          <h2>Zignix</h2>
-          <h3>Vai encarar o desafio?</h3>
-          <p>
-            Um jogo feito para testar sua inteligência. Descubra a palavra
-            secreta com o mínimo de pistas possíveis.
-          </p>
+          <h2>{i18n('home.gameBanner.title')}</h2>
+          <h3>{i18n('home.gameBanner.subtitle')}</h3>
+          <p>{i18n('home.gameBanner.text')}</p>
         </div>
 
         <div className={styles.gameBannerRightContent}>
