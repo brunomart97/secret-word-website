673cd5597eacf59a9d8d1491f765374b58f80226
Bruno Martins <brunomart97@hotmail.com>
2024-03-29 09:29:06 -0300

    feat: preparing the rules page

--- src/app/[locale]/rules/page.tsx
+++ src/app/[locale]/rules/page.tsx
@@ -1,3 +1,24 @@
+import { getTranslations } from 'next-intl/server'
+import type { Locale } from '../../../typings/Translate'
+
+import styles from './styles.module.scss'
+
+export async function generateMetadata({
+  params: { locale }
+}: {
+  params: { locale: Locale }
+}) {
+  const i18n = await getTranslations({ locale, namespace: 'i18n' })
+
+  return {
+    title: i18n('metaData.pages.rules.title')
+  }
+}
+
 export default function Rules() {
-  return <main>Rules</main>
+  return (
+    <main className={styles.rulesContainer}>
+      <h1>Rules</h1>
+    </main>
+  )
 }
